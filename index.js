7c52eeb5f10821758ad0c9e2e5ff1bc1fa98a6fe
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-01 09:56:29 -0800

    Continuing to mine through AJAX response. Implemented linting rules for project according to ryansobol lint rules. Rendering values from api state to the DOM.

--- index.js
+++ index.js
@@ -38,27 +38,32 @@ $('button').click(() => {
       success: (state) => {
         const events = state;
         const resultsDiv = $('.results');
-      for (const event of events) {
-        const showName = event.title;
-        const eventDateTime = event.formatted_datetime;
-        const ticketStatus = event.ticket_status;
-        const ticketURL = event.ticket_url;
-        // const eventID = event.id
-        const h5 = $('<h5>');
-        const h4 = $('<h4>');
-        const ticketLink = $('<a>');
-        ticketLink.prop('href', ticketURL);
-        ticketLink.text(ticketStatus);
-        h4.text(showName);
-        h5.text(eventDateTime);
-        resultsDiv.append(h4)
-        resultsDiv.append(h5)
-        resultsDiv.append(ticketLink)
-        console.log(showName)
-        console.log(event)
-      }
-
-
+        for (const event of events) {
+          const showName = event.title;
+          const eventDateTime = event.formatted_datetime;
+          const ticketStatus = event.ticket_status;
+          const ticketURL = event.ticket_url;
+          const eventLong = event.venue.longitude;
+          const eventLat = event.venue.latitude;
+          const venueLink = $('<a>');
+          // const eventID = event.id
+          const h5 = $('<h5>');
+          const h4 = $('<h4>');
+          const ticketLink = $('<a>');
+          ticketLink.prop('href', ticketURL);
+          venueLink.prop('href', `http://maps.google.com/maps?q=${eventLat},${eventLong}`).css('display', 'block');
+          venueLink.id = 'venue-link';
+          venueLink.text('Get Directions to the Venue');
+          ticketLink.text(ticketStatus);
+          h4.text(showName);
+          h5.text(eventDateTime);
+          resultsDiv.append(h4)
+          resultsDiv.append(h5)
+          resultsDiv.append(ticketLink)
+          resultsDiv.append(venueLink);
+          console.log(showName)
+          console.log(event)
+        }
       },
       dataType: 'jsonp'
     });
