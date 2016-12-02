ff2a40c260b5ca86c9846c4fae6969848038e5c5
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-02 13:18:05 -0800

    Basic materialize design rendering with nav-bar and footer. Mobile-first design.

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
 (function() {
   'use strict';
 
-  $('#search').on('focus', () => {
+  const clearSearch = () => {
     $('input[type=text], textarea').val('');
-  });
+  };
 
-  $('.search-button').click(() => {
-    $('.results').empty();
-    const getArtists = () => {
-      const input = $('input').val();
-
-      $('input[type=text], textarea').val('');
-      $.ajax({
-        type: 'GET',
-        url: `http://api.bandsintown.com/artists/${input}.json?api_version=2.0&app_id=michaelfriedman`,
-        success: (state) => {
-          const h3 = $('<h3>').text(state.name);
-          const resultsDiv = $('.results');
-          const img = $('<img>').prop('src', state.thumb_url).css('class', 'responsive-img');
-          const fbTour = $('<a>').prop('href', state.facebook_tour_dates_url);
-          const howManyEvents = $('<p>').text(`${state.name} has
-            ${state.upcoming_event_count} upcoming events`);
-          const fbPage = $('<a>').prop('href', state.facebook_page_url);
-
-          fbTour.text('Facebook Tour Page');
-          fbPage.text('Facebook Page');
-          resultsDiv
-          .append(h3)
-          .append(img)
-          .append(fbTour)
-          .append(howManyEvents)
-          .append(fbPage)
-          console.log(state);
-        },
-        dataType: 'jsonp'
-      });
-      getEvents(input);
-    };
-    getArtists();
-  });
   const getEvents = (input) => {
     $.ajax({
       type: 'GET',
@@ -48,35 +14,91 @@
         const resultsDiv = $('.results');
 
         for (const event of events) {
-          const showName = event.title;
-          const eventDateTime = event.formatted_datetime;
+          const showName = $('<h4>').text(event.title);
+          const eventDateTime = $('<h5>').text(event.formatted_datetime);
           const ticketStatus = event.ticket_status;
           const ticketURL = event.ticket_url;
           const eventLong = event.venue.longitude;
           const eventLat = event.venue.latitude;
-          const venueLink = $('<a>');
-          // const eventID = event.id
-          const h5 = $('<h5>');
-          const h4 = $('<h4>');
-          const ticketLink = $('<a>');
+          const venueLink = $('<a>').prop('href',
+            `http://maps.google.com/maps?q=${eventLat},${eventLong}`)
+            .css('display', 'block').text('Get Directions to the Venue');
+          // const eventID = event.id;
+          const ticketLink = $('<a>').prop('href', ticketURL)
+            .css('id', 'venue-link')
+            .text(ticketStatus);
 
-          ticketLink.prop('href', ticketURL);
-          venueLink.prop('href', `http://maps.google.com/maps?q=${eventLat},${eventLong}`).css('display', 'block');
-          venueLink.id = 'venue-link';
-          venueLink.text('Get Directions to the Venue');
-          ticketLink.text(ticketStatus);
-          h4.text(showName);
-          h5.text(eventDateTime);
           resultsDiv
-          .append(h4)
-          .append(h5)
+          .append(showName)
+          .append(eventDateTime)
           .append(ticketLink)
           .append(venueLink)
-          console.log(showName);
           console.log(event);
         }
       },
       dataType: 'jsonp'
     });
   };
+
+  const getArtists = (input) => {
+    $.ajax({
+      type: 'GET',
+      url: `http://api.bandsintown.com/artists/${input}.json?api_version=2.0&app_id=michaelfriedman`,
+      success: (state) => {
+        const h3 = $('<h3>').text(state.name);
+        const resultsDiv = $('.results');
+        const img = $('<img>').prop('src', state.thumb_url).css('class',
+          'responsive-img');
+        const fbTour = $('<a>').prop('href', state.facebook_tour_dates_url);
+        const howManyEvents = $('<p>').text(`${state.name} has
+          ${state.upcoming_event_count} upcoming events`);
+        const fbPage = $('<a>').prop('href', state.facebook_page_url);
+
+        fbTour.text('Facebook Tour Page');
+        fbPage.text('Facebook Page');
+        resultsDiv
+        .append(h3)
+        .append(img)
+        .append(fbTour)
+        .append(howManyEvents)
+        .append(fbPage)
+        console.log(state);
+      },
+      dataType: 'jsonp'
+    });
+    getEvents(input);
+  };
+
+  $('#search').on('focus', () => {
+    clearSearch();
+  });
+
+  $('#search').keyup((event) => {
+    const code = event.which;
+    const input = $('input').val();
+
+    if (code === 13) {
+      clearSearch();
+      if (input.trim() === '' || input.trim() === 'Enter Your Search Here') {
+        Materialize.toast('Please Enter an Artist or Group', 4000);
+
+        return;
+      }
+      $('.results').empty();
+      getArtists(input);
+    }
+  });
+
+  $('.search-button').click(() => {
+    const input = $('input').val();
+
+    clearSearch();
+    if (input.trim() === '' || input.trim() === 'Enter Your Search Here') {
+      Materialize.toast('Please Enter an Artist or Group', 4000);
+
+      return;
+    }
+    $('.results').empty();
+    getArtists(input);
+  });
 })();
