b285a2cf0b789a45cae05f0d27aab884b38fc595
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-04 10:00:22 -0800

    In-progress #9 create events template, error handling

--- index.js
+++ index.js
@@ -1,50 +1,81 @@
 (function() {
   'use strict';
 
+  $('.button-collapse').sideNav();
+
   const clearSearch = function() {
     $('input[type=text], textarea').val('');
   };
 
-  const createEventCard = function(events) {
-    console.log(events)
-
-    const collectionDiv = $('<ul>').prop('class', 'collection with-header');
-    const liHeader = $('<li>').prop('class', 'collection-header');
-    // liHeader.append('<h4>').text(events.title);
-    for (event of events) {
-      liHeader.append('<h4>').text(event.title);
-    }
-      collectionDiv.append(liHeader)
-    $('.results').append(collectionDiv);
-
-  };
+  // const createEventCard = function(events) {
+  //   // console.log(events)
+  //
+  //   const collectionDiv = $('<ul>').prop('class', 'collection with-header');
+  //   const liHeader = $('<li>').prop('class', 'collection-header');
+  //   const showTitle = $('<h4>')
+  //   liHeader.append(showTitle)
+  //   showTitle.text(event.title)
+  //   collectionDiv.append(liHeader)
+  // $('.results').append(collectionDiv);
+  //
+  //   for (const event of events) {
+  //     showTitle.text(event.title)
+  //     console.log(event.title)
+  //   }
+  //     collectionDiv.append(liHeader)
+  //   $('.results').append(collectionDiv);
+  //
+  // };
 
   const getEvents = function(input) {
     $.ajax({
       type: 'GET',
       url: `http://api.bandsintown.com/artists/${input}/events.json?api_version=2.0&app_id=michaelfriedman`,
       success: (state) => {
-        const events = state;
+        const shows = state;
         const resultsDiv = $('.results');
 
-        for (const event of events) {
-          const showName = $('<h4>').text(event.title);
-          const eventDateTime = $('<h5>').text(event.formatted_datetime);
-          const ticketStatus = event.ticket_status;
-          const ticketURL = event.ticket_url;
-          const eventLong = event.venue.longitude;
-          const eventLat = event.venue.latitude;
+        for (const show of shows) {
+          const showName = $('<h4>').text(show.title);
+          const showDateTime = $('<h5>').text(show.formatted_datetime);
+          const ticketStatus = `Tickets are ${show.ticket_status}.`;
+          const ticketURL = show.ticket_url;
+          const showLong = show.venue.longitude;
+          const showLat = show.venue.latitude;
           const venueLink = $('<a>').prop('href',
-            `http://maps.google.com/maps?q=${eventLat},${eventLong}`)
+            `http://maps.google.com/maps?q=${showLat},${showLong}`)
             .css('display', 'block').text('Get Directions to the Venue');
           // const eventID = event.id;
           const ticketLink = $('<a>').prop('href', ticketURL)
             .css('id', 'venue-link')
             .text(ticketStatus);
-          createEventCard(events)
+          // createEventCard(events)
+          const collectionDiv = $('<ul>').prop('class', 'collection with-header');
+          const liHeader = $('<li>').prop('class', 'collection-header');
+          const showTitle = $('<h4>');
+          const eventDateContainer = $('<li>').prop('class', 'collection-item');
+          let ticketsAvailableContainer;
+          show.ticket_url === null
+          ?
+            ticketsAvailableContainer = $('<li>').prop('href',
+              show.ticket_url).prop('class',
+              'collection-item').text(ticketStatus)
+          :
+            ticketsAvailableContainer = $('<a>').prop('href',
+              show.ticket_url).prop('class',
+              'collection-item').text(ticketStatus);
+
+          liHeader.append(showTitle);
+          eventDateContainer.text(show.formatted_datetime);
+          showTitle.text(show.title);
+          collectionDiv.append(liHeader)
+          .append(eventDateContainer)
+          .append(ticketsAvailableContainer);
+        $('.results').append(collectionDiv);
+
           resultsDiv
-          .append(showName)
-          .append(eventDateTime)
+          // .append(showName)
+          // .append(eventDateTime)
           .append(ticketLink)
           .append(venueLink)
           // console.log(event);
@@ -59,24 +90,30 @@
       type: 'GET',
       url: `http://api.bandsintown.com/artists/${input}.json?api_version=2.0&app_id=michaelfriedman`,
       success: (state) => {
-        const h3 = $('<h3>').text(state.name);
-        const resultsDiv = $('.results');
-        const img = $('<img>').prop('src', state.thumb_url).css('class',
-          'responsive-img');
-        const fbTour = $('<a>').prop('href', state.facebook_tour_dates_url);
-        const howManyEvents = $('<p>').text(`${state.name} has
-          ${state.upcoming_event_count} upcoming events`);
-        const fbPage = $('<a>').prop('href', state.facebook_page_url);
+        if (state.name === undefined) {
+          Materialize.toast('Sorry, no match found.', 4000);
+        }
+        else {
+          const h3 = $('<h3>').text(state.name);
+          const resultsDiv = $('.results');
+          const img = $('<img>').prop('src', state.thumb_url).css('class',
+            'responsive-img');
+          const fbTour = $('<a>').prop('href',
+            state.facebook_tour_dates_url);
+          const howManyEvents = $('<p>').text(`${state.name} has
+            ${state.upcoming_event_count} upcoming events`);
+          const fbPage = $('<a>').prop('href', state.facebook_page_url);
 
-        fbTour.text('Facebook Tour Page');
-        fbPage.text('Facebook Page');
-        resultsDiv
-        .append(h3)
-        .append(img)
-        .append(fbTour)
-        .append(howManyEvents)
-        .append(fbPage)
-        console.log(state);
+          fbTour.text('Facebook Tour Page');
+          fbPage.text('Facebook Page');
+          resultsDiv
+          .append(h3)
+          .append(img)
+          .append(fbTour)
+          .append(howManyEvents)
+          .append(fbPage);
+          console.log(state);
+        }
       },
       dataType: 'jsonp'
     });
