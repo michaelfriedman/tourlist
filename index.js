dce4b2d02793c96225c8dc67609baee73fca0bbe
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-05 11:24:01 -0800

    Added State Select Field to home page.

--- index.js
+++ index.js
@@ -2,9 +2,43 @@
   'use strict';
 
   $('.button-collapse').sideNav();
+  $('select').material_select();
 
   const clearSearch = function() {
     $('input[type=text], textarea').val('');
+  };
+
+  const renderEvents = function(state) {
+    const shows = state;
+    for (const show of shows) {
+      const ticketStatus = `Tickets are ${show.ticket_status}.`;
+      const collectionDiv = $('<ul>')
+        .prop('class', 'collection with-header');
+      const liHeader = $('<li>').prop('class', 'collection-header');
+      const showTitle = $('<h4>');
+      const showDateContainer = $('<li>').prop('class', 'collection-item');
+      const showDirectionsContainer = $('<a>')
+        .prop('class', 'collection-item')
+        .prop('href', `http://maps.google.com/maps?q=${show.venue.latitude},${show.venue.longitude}`)
+        .text('Directions to Venue');
+      let ticketsAvailableContainer;
+
+      show.ticket_url === null
+      ? ticketsAvailableContainer = $('<li>').prop('href',
+          show.ticket_url).prop('class',
+          'collection-item').text(ticketStatus)
+      : ticketsAvailableContainer = $('<a>').prop('href',
+          show.ticket_url).prop('class',
+          'collection-item').text('Buy Tickets');
+      liHeader.append(showTitle);
+      showDateContainer.text(show.formatted_datetime);
+      showTitle.text(show.title);
+      collectionDiv.append(liHeader)
+      .append(showDateContainer)
+      .append(ticketsAvailableContainer)
+      .append(showDirectionsContainer);
+      $('.results').append(collectionDiv);
+    }
   };
 
   const getEvents = function(input) {
@@ -12,48 +46,19 @@
       type: 'GET',
       url: `http://api.bandsintown.com/artists/${input}/events.json?api_version=2.0&app_id=michaelfriedman`,
       success: (state) => {
-        const shows = state;
-
-        for (const show of shows) {
-          const ticketStatus = `Tickets are ${show.ticket_status}.`;
-          const showLong = show.venue.longitude;
-          const showLat = show.venue.latitude;
-          const collectionDiv = $('<ul>')
-            .prop('class', 'collection with-header');
-          const liHeader = $('<li>').prop('class', 'collection-header');
-          const showTitle = $('<h4>');
-          const showDateContainer = $('<li>').prop('class', 'collection-item');
-          const showDirectionsContainer = $('<a>')
-            .prop('class', 'collection-item')
-            .prop('href', `http://maps.google.com/maps?q=${showLat},${showLong}`)
-            .text('Directions to Venue');
-          let ticketsAvailableContainer;
-
-          show.ticket_url === null
-          ? ticketsAvailableContainer = $('<li>').prop('href',
-              show.ticket_url).prop('class',
-              'collection-item').text(ticketStatus)
-          : ticketsAvailableContainer = $('<a>').prop('href',
-              show.ticket_url).prop('class',
-              'collection-item').text('Buy Tickets');
-          liHeader.append(showTitle);
-          showDateContainer.text(show.formatted_datetime);
-          showTitle.text(show.title);
-          collectionDiv.append(liHeader)
-          .append(showDateContainer)
-          .append(ticketsAvailableContainer)
-          .append(showDirectionsContainer);
-          $('.results').append(collectionDiv);
-        }
+        renderEvents(state);
       },
       dataType: 'jsonp'
     });
   };
 
   const createProfile = function(state) {
+    console.log(state);
     const fbPage = $('<a>').prop('href', state.facebook_page_url);
+    const fbTour = $('<a>').prop('href',
+        state.facebook_tour_dates_url).text('Facebook Tour Page');
     const rowDiv = $('<div>').prop('class', 'row');
-    const colDiv = $('<div>').prop('class', 'col s12 m7');
+    const colDiv = $('<div>').prop('class', 'col s12 m6 l6');
     const cardDiv = $('<div>').prop('class', 'card');
     const cardImageDiv = $('<div>').prop('class', 'card-image');
     const profileImg = $('<img>').prop('src', state.thumb_url);
@@ -62,8 +67,7 @@
     const linkDiv = $('<div>').prop('class', 'card-action');
     const cardLink = $('<a>')
       .prop('href', state.facebook_page_url).text('Facebook Page');
-    const howManyEvents = $('<p>').text(`${state.name} has
-      ${state.upcoming_event_count} upcoming events`);
+    const howManyEvents = $('<p>').text(`${state.name} Upcoming Events: ${state.upcoming_event_count}`);
 
     rowDiv.append(colDiv);
     colDiv.append(cardDiv);
@@ -71,7 +75,7 @@
     cardImageDiv.append(profileImg)
       .append(cardTitle);
     cardContent.append(howManyEvents);
-    linkDiv.append(cardLink);
+    linkDiv.append(cardLink).append(fbTour);
     fbPage.text('Facebook Page');
     $('.results')
     .append(rowDiv);
@@ -86,16 +90,7 @@
           Materialize.toast('Sorry, no match found.', 4000);
         }
         else {
-          const resultsDiv = $('.results');
-          const fbTour = $('<a>').prop('href',
-            state.facebook_tour_dates_url)
-            .prop('class', 'left')
-            .prop('display', 'block');
-
           createProfile(state);
-          resultsDiv
-          .append(fbTour)
-          .append(howManyEvents);
         }
       },
       dataType: 'jsonp'
