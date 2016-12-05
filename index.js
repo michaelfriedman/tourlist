91343c0dc6924b25fb5387058551c942e74370c4
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-04 17:14:49 -0800

    Close #5 create-artist-profile-template.  Rough draft profile finished, artist profiles are dynamically rendering.

--- index.js
+++ index.js
@@ -13,23 +13,11 @@
       url: `http://api.bandsintown.com/artists/${input}/events.json?api_version=2.0&app_id=michaelfriedman`,
       success: (state) => {
         const shows = state;
-        const resultsDiv = $('.results');
 
         for (const show of shows) {
-          const showDateTime = $('<h5>').text(show.formatted_datetime);
           const ticketStatus = `Tickets are ${show.ticket_status}.`;
-          const ticketURL = show.ticket_url;
           const showLong = show.venue.longitude;
           const showLat = show.venue.latitude;
-          const venueLink = $('<a>')
-            .prop('href',
-            `http://maps.google.com/maps?q=${showLat},${showLong}`)
-            .prop('display', 'block')
-            .text('Get Directions to the Venue');
-          // const eventID = event.id;
-          const ticketLink = $('<a>').prop('href', ticketURL)
-            .css('id', 'venue-link')
-            .text(ticketStatus);
           const collectionDiv = $('<ul>')
             .prop('class', 'collection with-header');
           const liHeader = $('<li>').prop('class', 'collection-header');
@@ -56,11 +44,37 @@
           .append(ticketsAvailableContainer)
           .append(showDirectionsContainer);
           $('.results').append(collectionDiv);
-          // console.log(event);
         }
       },
       dataType: 'jsonp'
     });
+  };
+
+  const createProfile = function(state) {
+    const fbPage = $('<a>').prop('href', state.facebook_page_url);
+    const rowDiv = $('<div>').prop('class', 'row');
+    const colDiv = $('<div>').prop('class', 'col s12 m7');
+    const cardDiv = $('<div>').prop('class', 'card');
+    const cardImageDiv = $('<div>').prop('class', 'card-image');
+    const profileImg = $('<img>').prop('src', state.thumb_url);
+    const cardTitle = $('<span>').prop('class', 'card-title').text(state.name);
+    const cardContent = $('<div>').prop('class', 'card-content');
+    const linkDiv = $('<div>').prop('class', 'card-action');
+    const cardLink = $('<a>')
+      .prop('href', state.facebook_page_url).text('Facebook Page');
+    const howManyEvents = $('<p>').text(`${state.name} has
+      ${state.upcoming_event_count} upcoming events`);
+
+    rowDiv.append(colDiv);
+    colDiv.append(cardDiv);
+    cardDiv.append(cardImageDiv).append(cardContent).append(linkDiv);
+    cardImageDiv.append(profileImg)
+      .append(cardTitle);
+    cardContent.append(howManyEvents);
+    linkDiv.append(cardLink);
+    fbPage.text('Facebook Page');
+    $('.results')
+    .append(rowDiv);
   };
 
   const getArtists = function(input) {
@@ -72,25 +86,16 @@
           Materialize.toast('Sorry, no match found.', 4000);
         }
         else {
-          const h3 = $('<h3>').text(state.name);
           const resultsDiv = $('.results');
-          const img = $('<img>').prop('src', state.thumb_url).css('class',
-            'responsive-img');
           const fbTour = $('<a>').prop('href',
-            state.facebook_tour_dates_url);
-          const howManyEvents = $('<p>').text(`${state.name} has
-            ${state.upcoming_event_count} upcoming events`);
-          const fbPage = $('<a>').prop('href', state.facebook_page_url);
+            state.facebook_tour_dates_url)
+            .prop('class', 'left')
+            .prop('display', 'block');
 
-          fbTour.text('Facebook Tour Page');
-          fbPage.text('Facebook Page');
+          createProfile(state);
           resultsDiv
-          .append(h3)
-          .append(img)
           .append(fbTour)
-          .append(howManyEvents)
-          .append(fbPage);
-          console.log(state);
+          .append(howManyEvents);
         }
       },
       dataType: 'jsonp'
